ece37af28fd03946ac666adde525c6debaed9c7b
George Papagapitos <george.g.papagapitos@gmail.com>
2021-07-28 14:11:43 -0500

    get my games working

--- routes/game.router.js
+++ routes/game.router.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 const Game = require('../models/Game');
 
+router.get('/', async (req, res) => {
+  try {
+    const games = await Game.find({});
+    res.status(200).json(games);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   const newGame = new Game(req.body);
   try {
